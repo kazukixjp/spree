52903d566c83402973e7b82fa0af491fd1b80a7b
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-11-15 07:51:44 +0100

    Remove migrations (moved to a separate branch)

--- core/db/migrate/20231114071427_create_promotion_batches.rb
+++
@@ -1,9 +0,0 @@
-class CreatePromotionBatches < ActiveRecord::Migration[7.1]
-  def change
-    create_table :spree_promotion_batches do |t|
-      t.string :status
-
-      t.timestamps
-    end
-  end
-end
--- core/db/migrate/20231114182646_add_promotion_batch_reference_to_promotions.rb
+++
@@ -1,5 +0,0 @@
-class AddPromotionBatchReferenceToPromotions < ActiveRecord::Migration[7.1]
-  def change
-    add_reference(:spree_promotions, :promotion_batch)
-  end
-end
