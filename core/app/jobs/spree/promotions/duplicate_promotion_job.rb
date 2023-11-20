ad39dcb04e29017a7301c033489b1b7b5d45461f
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-11-20 09:46:04 +0100

    Add promotion duplication job

---
+++ core/app/jobs/spree/promotions/duplicate_promotion_job.rb
@@ -0,0 +1,16 @@
+module Spree
+  module Promotions
+    class DuplicatePromotionJob < Spree::BaseJob
+      def perform(promotion_id)
+        promotion = find_promotion(promotion_id)
+        Spree::PromotionHandler::PromotionDuplicator.new(promotion).duplicate
+      end
+    
+      private
+    
+      def find_promotion(id)
+        Spree::Promotion.find(id)
+      end
+    end
+  end
+end
---
+++ core/spec/jobs/spree/promotions/duplicate_promotion_job_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+module Spree
+  describe Promotions::DuplicatePromotionJob do
+    describe "#perform" do
+      subject(:execute_job) { described_class.new.perform(promotion.id) }
+  
+      let(:promotion) { build(:promotion) }
+  
+      let(:double) { instance_double(Spree::PromotionHandler::PromotionDuplicator) }
+  
+      before do
+        allow(Spree::Promotion)
+          .to receive(:find)
+          .and_return(promotion)
+      end
+  
+      it "sends #duplicate to the duplicator service" do
+        expect(Spree::PromotionHandler::PromotionDuplicator)
+          .to receive(:new)
+          .with(promotion)
+          .and_return(double)
+        expect(double)
+          .to receive(:duplicate)
+  
+          execute_job
+      end
+    end
+  end
+end
