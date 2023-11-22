d696b56d5614d2f1d338c2ae2f2c6118557dc75b
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-11-22 15:21:33 +0100

    Pass PromotionBatch id to duplication job

--- core/app/jobs/spree/promotions/duplicate_promotion_job.rb
+++ core/app/jobs/spree/promotions/duplicate_promotion_job.rb
@@ -1,9 +1,9 @@
 module Spree
   module Promotions
     class DuplicatePromotionJob < Spree::BaseJob
-      def perform(promotion_id)
+      def perform(promotion_id, promotion_batch_id)
         promotion = find_promotion(promotion_id)
-        Spree::PromotionHandler::PromotionBatchDuplicator.new(promotion).duplicate
+        Spree::PromotionHandler::PromotionBatchDuplicator.new(promotion, promotion_batch_id).duplicate
       end
 
       private
--- core/spec/jobs/spree/promotions/duplicate_promotion_job_spec.rb
+++ core/spec/jobs/spree/promotions/duplicate_promotion_job_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 module Spree
   describe Promotions::DuplicatePromotionJob do
     describe "#perform" do
-      subject(:execute_job) { described_class.new.perform(promotion.id) }
+      subject(:execute_job) { described_class.new.perform(promotion.id, batch.id) }
 
       let(:promotion) { build(:promotion) }
-
-      let(:double) { instance_double(Spree::PromotionHandler::PromotionDuplicator) }
+      let(:batch) { instance_double(Spree::PromotionBatch, id: 123) }
+      let(:duplicator) { instance_double(Spree::PromotionHandler::PromotionDuplicator) }
 
       before do
         allow(Spree::Promotion)
@@ -18,9 +18,9 @@ module Spree
       it "sends #duplicate to the duplicator service" do
         expect(Spree::PromotionHandler::PromotionBatchDuplicator)
           .to receive(:new)
-          .with(promotion)
-          .and_return(double)
-        expect(double)
+          .with(promotion, batch.id)
+          .and_return(duplicator)
+        expect(duplicator)
           .to receive(:duplicate)
 
           execute_job
