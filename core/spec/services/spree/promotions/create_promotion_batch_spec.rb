ca7e117f052959acc945e09d53ab6c3a26ef8d2f
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-11-22 19:39:14 +0100

    Adjust service test

--- core/spec/services/spree/promotions/create_promotion_batch_spec.rb
+++ core/spec/services/spree/promotions/create_promotion_batch_spec.rb
@@ -3,22 +3,38 @@ require 'spec_helper'
 module Spree
   describe Promotions::CreatePromotionBatch do
     describe "#call" do
-      subject(:batch) { described_class.call(promotion, size) }
+      subject(:create_promotion_batch) { described_class.call(promotion, size) }
 
-      let(:promotion) { create(:promotion) }
+      let(:promotion) { build(:promotion) }
+      let(:promotion_batch) { instance_double(Spree::PromotionBatch, id: 123) }
       let(:size) { 50 }
 
-      it "creates a PromotioBatch" do
-        expect{ batch }.to change { Spree::PromotionBatch.count }.by(1)
+      before do
+        allow(Spree::Promotion)
+          .to receive(:find)
+          .and_return(promotion)
+        allow(Spree::PromotionBatch)
+          .to receive(:create!)
+          .and_return(promotion_batch)
+        allow(Spree::Promotions::DuplicatePromotionJob)
+          .to receive(:perform_later)
+          .with(promotion.id, promotion_batch.id)
+      end
+
+      it "creates a PromotionBatch" do
+        expect(Spree::PromotionBatch)
+          .to receive(:create!)
+
+          create_promotion_batch
       end
 
       it "enqueues DuplicatePromotionJob jobs", sidekiq: :inline do
         expect(Spree::Promotions::DuplicatePromotionJob)
           .to receive(:perform_later)
           .at_least(50).times
-          .with(promotion.id)
+          .with(promotion.id, promotion_batch.id)
 
-        batch
+        create_promotion_batch
       end
     end
   end
