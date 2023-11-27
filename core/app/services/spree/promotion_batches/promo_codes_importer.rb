cc6394172174766f4337463d1a52f88a51fd4ea5
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-11-27 17:04:58 +0100

    Add logic for promo codes importing

--- core/app/services/spree/promotion_batches/promo_codes_importer.rb
+++ core/app/services/spree/promotion_batches/promo_codes_importer.rb
@@ -1,10 +1,30 @@
 module Spree
   module PromotionBatches
     class PromoCodesImporter
-      def initialize(params)
+      def initialize(content:, promotion_batch_id:)
+        @content = content
+        @promotion_batch = find_promotion_batch(promotion_batch_id)
       end
 
       def call
+        parsed_rows.each do |parsed_row|
+          Spree::Promotions::DuplicatePromotionJob
+            .perform_later(@promotion_batch.template_promotion_id, @promotion_batch.id, code: parsed_row)
+        end
+      end
+
+      private
+
+      def find_promotion_batch(id)
+        Spree::PromotionBatch.find(id)
+      end
+
+      def parsed_rows
+        CSV.new(rows, headers: false).read.map(&:first)
+      end
+
+      def rows
+        @content.lines.join
       end
     end  
   end
--- core/spec/services/spree/promotion_batches/promo_codes_importer_spec.rb
+++ core/spec/services/spree/promotion_batches/promo_codes_importer_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 module Spree
   describe PromotionBatches::PromoCodesImporter do
     subject(:import_promo_codes) do
-      described_class.new(params).call
+      described_class.new(content: content, promotion_batch_id: promotion_batch.id).call
     end
 
+    let(:promotion) { create(:promotion) }
+    let(:promotion_batch) { create(:promotion_batch, template_promotion_id: promotion.id) }
     let(:code1) { 'cf14cec8' }
     let(:code2) { '4b2ff1a7' }
     let(:content) do
@@ -15,16 +17,13 @@ module Spree
       CSV
     end
 
-    before do
-    end
-
     it "enqueues DuplicatePromotionJob jobs", sidekiq: :inline do
       expect(Spree::Promotions::DuplicatePromotionJob)
         .to receive(:perform_later)
-        .with(promotion.id, promotion_batch.id, code1)
+        .with(promotion.id, promotion_batch.id, code: code1)
       expect(Spree::Promotions::DuplicatePromotionJob)
         .to receive(:perform_later)
-        .with(promotion.id, promotion_batch.id, code2)
+        .with(promotion.id, promotion_batch.id, code: code2)
 
       import_promo_codes
     end
