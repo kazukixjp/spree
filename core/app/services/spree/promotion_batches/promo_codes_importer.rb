07c692044dce2c8e3cfb298c5a6d3dcbeaf68b78
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-11-27 21:08:16 +0100

    Add file validation

--- core/app/services/spree/promotion_batches/promo_codes_importer.rb
+++ core/app/services/spree/promotion_batches/promo_codes_importer.rb
@@ -1,12 +1,16 @@
 module Spree
   module PromotionBatches
     class PromoCodesImporter
+      Error = Class.new(StandardError)
+
       def initialize(content:, promotion_batch_id:)
         @content = content
         @promotion_batch = find_promotion_batch(promotion_batch_id)
       end
 
       def call
+        validate_file!
+
         parsed_rows.each do |parsed_row|
           Spree::Promotions::DuplicatePromotionJob
             .perform_later(@promotion_batch.template_promotion_id, @promotion_batch.id, code: parsed_row)
@@ -17,6 +21,10 @@ module Spree
 
       def find_promotion_batch(id)
         Spree::PromotionBatch.find(id)
+      end
+
+      def validate_file!
+        raise Error, "No file / Empty file" if parsed_rows.blank?
       end
 
       def parsed_rows
