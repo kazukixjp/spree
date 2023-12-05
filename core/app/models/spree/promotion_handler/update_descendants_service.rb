6ba48f56b12cf0c1e76ea03a74301d1c3bc8265d
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-12-05 21:42:35 +0100

    Add draft of a service for handling promotion template updates.

--- core/app/models/spree/promotion_handler/update_descendants_service.rb
+++ core/app/models/spree/promotion_handler/update_descendants_service.rb
@@ -2,9 +2,46 @@ module Spree
   module PromotionHandler
     class UpdateDescendantsService
       def initialize(template_promotion)
+        @template_promotion = template_promotion
+        @promotions_to_destroy = [] # this needs to get populated
       end
 
       def call
+        return unless promotion_batches?
+
+        ids_and_codes_by_batch.each do |batch|
+          batch.each do |batch_id, promotions_data|
+            promotions_data.each do |data|
+              Job.perform_later(data[:id], batch_id, data[:code])
+            end
+          end
+        end
+      end
+
+      private
+
+      def promotion_batches?
+        promotion_batches.any?
+      end
+
+      def promotion_batches
+        @promotion_batches = Spree::PromotionBatch.where(template_promotion_id: @template_promotion.id)
+      end
+
+      def ids_and_codes_by_batch
+        @promotion_batches.map do |batch|
+          {batch.id => unused_codes(batch)}
+        end
+      end
+
+      def unused_codes(batch)
+        unused_promotions(batch).map {|promotion| {id: promotion.id, code: promotion.code}}
+      end
+
+      def unused_promotions(batch)
+        batch.promotions
+          .includes(:promotion_actions)
+          .select {|promotion| promotion.credits_count < promotion.usage_limit}
       end
     end
   end
--- core/spec/models/spree/promotion_handler/update_descendants_service_spec.rb
+++ core/spec/models/spree/promotion_handler/update_descendants_service_spec.rb
@@ -2,4 +2,37 @@ require 'spec_helper'
 
 describe Spree::PromotionHandler::UpdateDescendantsService do
   subject { described_class.new(promotion).call }
+
+  let(:promotion) { build(:promotion) }
+  let(:batches) { [build(:promotion_batch)] }
+
+  context 'when the promotion is a template for a batch' do
+    before do
+      allow(Spree::PromotionBatch)
+        .to receive(where)
+        .with(template_promotion_id: promotion.id )
+        .and_return(batches)
+    end
+
+    it 'enqueues a job' do
+      expect(Job).to receive(:perform_later)
+
+      subject
+    end
+  end
+
+  context 'when the promotion is NOT a template for a batch' do
+    before do
+      allow(Spree::PromotionBatch)
+        .to receive(where)
+        .with(template_promotion_id: promotion.id )
+        .and_return([])
+    end
+
+    it 'does not enqueue a job' do
+      expect(Job).not_to receive(:perform_later)
+
+      subject
+    end
+  end
 end
