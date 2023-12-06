41b680b95f9a1073d51a57b465aaa2905f48ab18
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-12-06 18:37:58 +0100

    Rework service for updating descendant promotions

--- core/app/models/spree/promotion_handler/update_descendants_service.rb
+++ core/app/models/spree/promotion_handler/update_descendants_service.rb
@@ -3,16 +3,19 @@ module Spree
     class UpdateDescendantsService
       def initialize(template_promotion)
         @template_promotion = template_promotion
-        @promotions_to_destroy = [] # this needs to get populated
       end
 
       def call
         return unless promotion_batches?
 
-        ids_and_codes_by_batch.each do |batch|
-          batch.each do |batch_id, promotions_data|
-            promotions_data.each do |data|
-              Job.perform_later(data[:id], batch_id, data[:code])
+        unused_promotions_ids_by_batch.each do |batch|
+          batch.each do |batch_id, promotion_ids|
+            promotion_ids.each do |promotion_id|
+              Spree::Promotions::HandleDescendantPromotionJob.perform_later(
+                template_promotion_id: @template_promotion.id,
+                descendant_promotion_id: promotion_id,
+                promotion_batch_id: batch_id
+              )
             end
           end
         end
@@ -28,20 +31,17 @@ module Spree
         @promotion_batches = Spree::PromotionBatch.where(template_promotion_id: @template_promotion.id)
       end
 
-      def ids_and_codes_by_batch
+      def unused_promotions_ids_by_batch
         @promotion_batches.map do |batch|
-          {batch.id => unused_codes(batch)}
+          {batch.id => unused_promotions_ids(batch)}
         end
       end
 
-      def unused_codes(batch)
-        unused_promotions(batch).map {|promotion| {id: promotion.id, code: promotion.code}}
-      end
-
-      def unused_promotions(batch)
+      def unused_promotions_ids(batch)
         batch.promotions
           .includes(:promotion_actions)
           .select {|promotion| promotion.credits_count < promotion.usage_limit}
+          .pluck :id
       end
     end
   end
--- core/spec/models/spree/promotion_handler/update_descendants_service_spec.rb
+++ core/spec/models/spree/promotion_handler/update_descendants_service_spec.rb
@@ -1,21 +1,33 @@
 require 'spec_helper'
 
 describe Spree::PromotionHandler::UpdateDescendantsService do
-  subject { described_class.new(promotion).call }
+  subject { described_class.new(template_promotion).call }
 
-  let(:promotion) { build(:promotion) }
-  let(:batches) { [build(:promotion_batch)] }
+  let(:template_promotion) { build(:promotion) }
+  let(:descendant_promotion) { build(:promotion) }
+  let(:batch) { build(:promotion_batch) }
+  let(:batches) { [batch] }
+  let(:promotions) { [descendant_promotion] }
 
   context 'when the promotion is a template for a batch' do
     before do
       allow(Spree::PromotionBatch)
         .to receive(where)
-        .with(template_promotion_id: promotion.id )
+        .with(template_promotion_id: template_promotion.id )
         .and_return(batches)
+      allow(batch)
+        .to receive(:promotions)
+        .and_return(promotions)
     end
 
     it 'enqueues a job' do
-      expect(Job).to receive(:perform_later)
+      expect(Spree::Promotions::HandleDescendantPromotionJob)
+        .to receive(:perform_later)
+        .with(
+          template_promotion_id: template_promotion.id
+          descendant_promotion_id: descendant_promotion.id
+          promotion_batch_id: batch.id
+        )
 
       subject
     end
@@ -25,12 +37,12 @@ describe Spree::PromotionHandler::UpdateDescendantsService do
     before do
       allow(Spree::PromotionBatch)
         .to receive(where)
-        .with(template_promotion_id: promotion.id )
+        .with(template_promotion_id: template_promotion.id )
         .and_return([])
     end
 
     it 'does not enqueue a job' do
-      expect(Job).not_to receive(:perform_later)
+      expect(Spree::Promotions::HandleDescendantPromotionJob).not_to receive(:perform_later)
 
       subject
     end
