8a65bafff18a92edd5d42880bb29f823ac71d63a
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-12-06 20:19:50 +0100

    Fix test setup

--- core/spec/jobs/spree/promotions/handle_descendant_promotion_job_spec.rb
+++ core/spec/jobs/spree/promotions/handle_descendant_promotion_job_spec.rb
@@ -4,22 +4,28 @@ module Spree
   describe Promotions::HandleDescendantPromotionJob do
     describe "#perform" do
       let(:template_promotion) { build(:promotion) }
-      let(:promotion) { build(:promotion) }
+      let(:descendant_promotion) { build(:promotion) }
       let(:batch) { instance_double(Spree::PromotionBatch, id: 123) }
-      let(:duplicator) { instance_double(Spree::PromotionHandler::XXXX) }
+      let(:duplicator) { instance_double(Spree::PromotionHandler::PromotionBatchUpdateHandler) }
 
       before do
         allow(Spree::Promotion)
           .to receive(:find)
-          .and_return(template_promotion, promotion)
+          .and_return(template_promotion, descendant_promotion)
       end
 
-      subject(:execute_job) { described_class.new.perform(template_promotion.id, batch.id, promotion.id) }
+      subject(:execute_job) do
+        described_class.new.perform(
+          template_promotion_id: template_promotion.id,
+          descendant_promotion_id: descendant_promotion.id,
+          promotion_batch_id: batch.id
+        )
+      end
 
       it "sends #duplicate to the duplicator service" do
-        expect(Spree::PromotionHandler::XXXX)
+        expect(Spree::PromotionHandler::PromotionBatchUpdateHandler)
           .to receive(:new)
-          .with(template_promotion, batch.id, promotion)
+          .with(template_promotion, batch.id, descendant_promotion)
           .and_return(duplicator)
         expect(duplicator)
           .to receive(:duplicate)
