8c4f6b018b661d9b3a1d039f68b3011fcf4a29f0
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-12-06 21:15:31 +0100

    Fix variable naming. Cleanup.

--- core/app/models/spree/promotion_handler/promotion_batch_update_handler.rb
+++ core/app/models/spree/promotion_handler/promotion_batch_update_handler.rb
@@ -1,35 +1,28 @@
 module Spree
   module PromotionHandler
     class PromotionBatchUpdateHandler < Spree::PromotionDuplicatorCore
-      def initialize(template_promotion, promotion_batch_id, descendant_promotion)
-        @template_promotion = template_promotion
+      def initialize(promotion, promotion_batch_id, descendant_promotion)
+        @promotion = promotion
         @promotion_batch_id = promotion_batch_id
         @descendant_promotion = descendant_promotion
       end
 
       def duplicate
-        @new_promotion = @template_promotion.dup
+        @new_promotion = @promotion.dup
         @new_promotion.usage_limit = 1
         @new_promotion.promotion_batch_id = @promotion_batch_id
-        @new_promotion.path = "#{@template_promotion.path}_#{@random_string}"
-        code_assignment
-        @new_promotion.stores = @template_promotion.stores
+        @new_promotion.path = @descendant_promotion.path
+        @new_promotion.code = @descendant_promotion.code
+        @new_promotion.stores = @promotion.stores
 
         ActiveRecord::Base.transaction do
           @new_promotion.save
           copy_rules
           copy_actions
+          @descendant_promotion.destroy!
         end
 
         @new_promotion
-      end
-
-      private
-
-      def code_assignment
-        return @new_promotion.generate_code=(true) unless @code
-
-        @new_promotion.code = @code
       end
     end
   end
