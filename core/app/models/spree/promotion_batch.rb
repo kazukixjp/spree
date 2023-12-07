c9d294f607b8f4b245d28cab0795c597bacc858d
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-12-07 18:47:03 +0100

    Add presenter methods for PromotionBatch

--- core/app/models/spree/promotion_batch.rb
+++ core/app/models/spree/promotion_batch.rb
@@ -2,5 +2,17 @@ module Spree
   class PromotionBatch < Spree::Base
     has_many :promotions
     belongs_to :template_promotion, class_name: "Promotion"
+
+    def template_promotion_name_id
+      base = template_promotion.name
+      addon = template_promotion_id.to_s
+      base.concat(' # ', addon)
+    end
+
+    def model_name_id
+      base = self.class.name.demodulize
+      addon = self.id.to_s
+      base.concat(' # ', addon)
+    end
   end
 end
