5639ec0930a9edb6dc8307aa4b2df620100a5add
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-12-08 18:45:50 +0100

    Add validation of template promotion assignment on update.

--- core/app/models/spree/promotion_batch.rb
+++ core/app/models/spree/promotion_batch.rb
@@ -1,5 +1,7 @@
 module Spree
   class PromotionBatch < Spree::Base
+    validate :template_assignment, on: :update
+
     has_many :promotions
     belongs_to :template_promotion, class_name: "Promotion"
 
@@ -13,6 +15,14 @@ module Spree
       base = self.class.name.demodulize
       addon = self.id.to_s
       base.concat(' # ', addon)
+    end
+
+    private
+
+    def template_assignment
+      if template_promotion_id_changed? && template_promotion_id_was.present?
+        errors.add(:template_promotion_id, :template_promotion_already_assigned, message: Spree.t(:template_promotion_already_assigned))
+      end
     end
   end
 end
