f4b973ffc8301acebc0a04559411391281fff62c
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2023-12-11 15:35:36 +0100

    Rework DuplicatePromotionJob to not allow for generating a code that is present in the current batch.

--- core/app/jobs/spree/promotions/duplicate_promotion_job.rb
+++ core/app/jobs/spree/promotions/duplicate_promotion_job.rb
@@ -1,15 +1,46 @@
 module Spree
   module Promotions
     class DuplicatePromotionJob < Spree::BaseJob
-      def perform(promotion_id, promotion_batch_id, code: nil)
-        promotion = find_promotion(promotion_id)
-        Spree::PromotionHandler::PromotionBatchDuplicator.new(promotion, promotion_batch_id, code: code).duplicate
+      def perform(config)
+        promotion = find_instance(config[:template_promotion_id], model: Spree::Promotion)
+        code = applicable_code(config)
+
+        Spree::PromotionHandler::PromotionBatchDuplicator.new(promotion, config[:id], code: code).duplicate
       end
 
       private
 
-      def find_promotion(id)
-        Spree::Promotion.find(id)
+      def find_instance(id, model:)
+        model.find(id)
+      end
+
+      def applicable_code(config)
+        promotion_batch = find_instance(config[:id], model: Spree::PromotionBatch)
+        codes = codes(promotion_batch)
+        code_generator = code_generator(config)
+
+        loop do
+          candidate = generate_code(code_generator)
+          break candidate if candidate_valid?(candidate, codes)
+        end
+      end
+
+      def codes(promotion_batch)
+        promotion_batch.promotions.pluck :code
+      end
+
+      def code_generator(config)
+        CodeGenerator.new(config)
+      end
+
+      def generate_code(generator)
+        generator.build
+      end
+
+      def candidate_valid?(candidate, codes)
+        return true unless codes.include?(candidate)
+
+        false
       end
     end
   end
--- core/spec/jobs/spree/promotions/duplicate_promotion_job_spec.rb
+++ core/spec/jobs/spree/promotions/duplicate_promotion_job_spec.rb
@@ -3,45 +3,62 @@ require 'spec_helper'
 module Spree
   describe Promotions::DuplicatePromotionJob do
     describe "#perform" do
-      let(:promotion) { build(:promotion) }
-      let(:batch) { instance_double(Spree::PromotionBatch, id: 123) }
+      let(:promotion) { build(:promotion, code: existing_code) }
+      let(:promotion_batch) { build(:promotion_batch, template_promotion: promotion) }
       let(:duplicator) { instance_double(Spree::PromotionHandler::PromotionDuplicator) }
+      let(:code_generator) { instance_double(Promotions::CodeGenerator) }
+      let(:existing_code) { "existing_code" }
+      let(:new_code) { 'new_code' }
+
+      let(:config) do
+        {
+          template_promotion_id: 1,
+          id: 2,
+          batch_size: 3,
+          affix: :prefix,
+          content: 'blackweek',
+          deny_list: %w(forbidden words)
+        }
+      end
 
       before do
         allow(Spree::Promotion)
           .to receive(:find)
+          .with(config[:template_promotion_id])
           .and_return(promotion)
+        allow(Spree::PromotionBatch)
+          .to receive(:find)
+          .with(config[:id])
+          .and_return(promotion_batch)
+        allow(promotion_batch)
+          .to receive(:promotions)
+          .and_return([promotion])
+        allow(Promotions::CodeGenerator)
+          .to receive(:new)
+          .with(config)
+          .and_return(code_generator)
+        allow(code_generator)
+          .to receive(:build)
+          .and_return(existing_code, new_code)
+        allow(Spree::PromotionHandler::PromotionBatchDuplicator)
+          .to receive(:new)
+          .with(promotion, config[:id], code: new_code)
+          .and_return(duplicator)
+        allow(duplicator)
+          .to receive(:duplicate)
       end
 
-      context "when code is provided" do
-        subject(:execute_job) { described_class.new.perform(promotion.id, batch.id, code: code) }
-        let(:code) { 'ed4517b3' }
+      subject(:execute_job) { described_class.new.perform(config) }
 
-        it "sends #duplicate to the duplicator service" do
-          expect(Spree::PromotionHandler::PromotionBatchDuplicator)
-            .to receive(:new)
-            .with(promotion, batch.id, code: code)
-            .and_return(duplicator)
-          expect(duplicator)
-            .to receive(:duplicate)
+      it "sends #duplicate to the duplicator service" do
+        expect(Spree::PromotionHandler::PromotionBatchDuplicator)
+          .to receive(:new)
+          .with(promotion, config[:id], code: new_code)
+          .and_return(duplicator)
+        expect(duplicator)
+          .to receive(:duplicate)
 
-          execute_job
-        end
-      end
-
-      context "when code is  NOT provided" do
-        subject(:execute_job) { described_class.new.perform(promotion.id, batch.id) }
-
-        it "sends #duplicate to the duplicator service" do
-          expect(Spree::PromotionHandler::PromotionBatchDuplicator)
-            .to receive(:new)
-            .with(promotion, batch.id, code: nil)
-            .and_return(duplicator)
-          expect(duplicator)
-            .to receive(:duplicate)
-
-          execute_job
-        end
+        execute_job
       end
     end
   end
