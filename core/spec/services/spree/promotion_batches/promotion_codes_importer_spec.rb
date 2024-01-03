131bec0df48ea11aa0a36a602aa6e40255fd11a9
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2024-01-03 21:57:16 +0100

    Supplement PromotionCodesImporter spec

--- core/spec/services/spree/promotion_batches/promotion_codes_importer_spec.rb
+++ core/spec/services/spree/promotion_batches/promotion_codes_importer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Spree
   describe PromotionBatches::PromotionCodesImporter do
     subject(:import_promo_codes) do
-      described_class.new(content: content, promotion_batch_id: promotion_batch.id).call
+      described_class.new(file: file, promotion_batch_id: promotion_batch.id).call
     end
 
     let(:promotion_batch) { build(:promotion_batch) }
@@ -17,8 +17,16 @@ module Spree
         #{code2}
       CSV
     end
+    let(:file) { double }
+    let(:content_type) { 'text/csv' }
 
     before do
+      allow(file)
+        .to receive(:content_type)
+        .and_return(content_type)
+      allow(file)
+        .to receive(:read)
+        .and_return(content)
       allow(Spree::PromotionBatch)
         .to receive(:find)
         .and_return(promotion_batch)
@@ -30,15 +38,33 @@ module Spree
         .and_return(batch_id)
     end
 
-    it "enqueues DuplicatePromotionJob jobs", sidekiq: :inline do
-      expect(Spree::Promotions::DuplicatePromotionJob)
-        .to receive(:perform_later)
-        .with(template_promotion_id: template_promotion_id, batch_id: batch_id, code: code1)
-      expect(Spree::Promotions::DuplicatePromotionJob)
-        .to receive(:perform_later)
-        .with(template_promotion_id: template_promotion_id, batch_id: batch_id, code: code2)
+    context "when file is correct" do
+      it "enqueues DuplicatePromotionJob jobs", sidekiq: :inline do
+        expect(Spree::Promotions::DuplicatePromotionJob)
+          .to receive(:perform_later)
+          .with(template_promotion_id: template_promotion_id, batch_id: batch_id, code: code1)
+        expect(Spree::Promotions::DuplicatePromotionJob)
+          .to receive(:perform_later)
+          .with(template_promotion_id: template_promotion_id, batch_id: batch_id, code: code2)
 
-      import_promo_codes
+        import_promo_codes
+      end
+    end
+
+    context "when file has wrong content type" do
+      let(:content_type) { 'unallowable_type' }
+
+      it "raises an error" do
+        expect { import_promo_codes }.to raise_error Spree::PromotionBatches::PromotionCodesImporter::Error
+      end
+    end
+
+    context "when file is empty" do
+      let(:content) { "" }
+
+      it "raises an error" do
+        expect { import_promo_codes }.to raise_error Spree::PromotionBatches::PromotionCodesImporter::Error
+      end
     end
   end
 end
