7fc3e56b5cef802fd6baa5a61737ae40c39ef6b8
Tomasz Donarski <tomasz.donarski.dev@gmail.com>
2024-01-06 10:28:49 +0100

    Supplement PromotionBatches platform API tests

--- api/spec/requests/spree/api/v2/platform/promotion_batches_spec.rb
+++ api/spec/requests/spree/api/v2/platform/promotion_batches_spec.rb
@@ -22,6 +22,22 @@ describe 'Promotion API v2 spec', type: :request do
         expect(json_response['data']).to have_relationship(:template_promotion).with_data(nil)
       end
     end
+
+    context 'when assigning a non-existing template' do
+      before { post '/api/v2/platform/promotion_batches', params: params, headers: bearer_token }
+    
+      let(:new_promotion_batch_attributes) do
+        {
+          template_promotion_id: 123
+        }
+      end
+  
+      let(:params) { { promotion_batch: new_promotion_batch_attributes } }
+  
+      it 'does not create and returns an error' do
+        expect(json_response.has_key?('error')).to eq(true)
+      end
+    end
   end
 
   describe 'promotion_batches#update' do
@@ -41,6 +57,31 @@ describe 'Promotion API v2 spec', type: :request do
 
       it 'updates and returns a promotion batch' do
         expect(json_response['data']).to have_relationship(:template_promotion).with_data({ 'id' => existing_promotion.id.to_s, 'type' => 'promotion' })
+      end
+    end
+
+    context 'when trying to override an assigned template' do
+      before do
+        existing_promotion
+        other_existing_promotion
+        existing_promotion_batch
+        put "/api/v2/platform/promotion_batches/#{existing_promotion_batch.id}", params: params, headers: bearer_token
+      end
+
+      let(:existing_promotion) { create(:promotion) }
+      let(:other_existing_promotion) { create(:promotion) }
+      let(:existing_promotion_batch) { create(:promotion_batch, template_promotion: existing_promotion) }
+
+      let(:update_promotion_attributes) do
+        {
+          template_promotion_id: other_existing_promotion.id
+        }
+      end
+
+      let(:params) { { promotion_batch: update_promotion_attributes } }
+
+      it 'does not update and returns an error' do
+        expect(json_response.dig(:errors, :template_promotion_id)).to eq(['Template promotion has already been assigned!'])
       end
     end
   end
